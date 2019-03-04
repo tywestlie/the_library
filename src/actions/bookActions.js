3c95f5c43fcb7de5f4ad7fed0e3df6c44d182efd
Tyler Westlie <tyler.westlie@gmail.com>
2019-03-04 13:05:45 -0700

    removes default styling

--- src/actions/bookActions.js
+++ src/actions/bookActions.js
@@ -3,3 +3,7 @@ import { FETCH_BOOKS, NEW_BOOK } from './types';
 export const fetchBooks = () => dispatch => {
   console.log('hi hi hi hi')
 }
+
+export const addBook = (book) => dispatch => {
+  
+}
