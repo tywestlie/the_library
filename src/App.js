de1da6f60f10d334e3bf1bda5ac1fa67066acfbc
Tyler Westlie <tyler.westlie@gmail.com>
2019-03-05 16:21:36 -0700

    removes commented code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-// import { Provider } from 'react-redux';
 
 import AllBooks from "./components/AllBooks/AllBooks"
-
-// import store from './store';
-
 
 class App extends Component {
   render() {
     return (
-      // <Provider store={store}>
         <div className="App">
         <AllBooks />
         </div>
-      // </Provider>
     );
   }
 }
